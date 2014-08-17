ef0705f5b77ceb1a232ea38605cfdfaaa6aa6021
lycc001 <lyccemail@163.com>
2014-08-17 10:17:44 +0800

    add a btn method

--- src/io/SeanChense/ops/HandlePicActivity.java
+++ src/io/SeanChense/ops/HandlePicActivity.java
@@ -5,11 +5,18 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class HandlePicActivity extends Activity {
+	private final static String Tag = "HandlePicActivity";
+	private final static String Today = "I am here with you";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,18 @@ public class HandlePicActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,                
                                        WindowManager.LayoutParams. FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_handle_pic);
+		ImageButton btn_save = (ImageButton)findViewById(R.id.btn_save);
+		btn_save.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//Store the picture
+				Log.i(Tag, Today+"btn_Save clicked");
+				
+			}
+			
+			
+		});
 		String path = getPath();
 		Bitmap yourSelectedImage = BitmapFactory.decodeFile(path);
 		((ImageView)findViewById(R.id.image)).setImageBitmap(yourSelectedImage);
