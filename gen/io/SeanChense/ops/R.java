db2fdeeee5ffca30230beb46768f3a7d9adfe9cf
lycc001 <lyccemail@163.com>
2014-08-17 10:06:03 +0800

    完善 UI

--- gen/io/SeanChense/ops/R.java
+++ gen/io/SeanChense/ops/R.java
@@ -31,17 +31,24 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int handleImageMarginX=0x7f050002;
+        public static final int handleImageMarginY=0x7f050003;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int example=0x7f020000;
+        public static final int handle_bg=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int save=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int action_settings=0x7f090007;
+        public static final int btn_save=0x7f090006;
         public static final int dummy_button=0x7f090003;
         public static final int flayout=0x7f090000;
         public static final int fullscreen_content=0x7f090001;
         public static final int fullscreen_content_controls=0x7f090002;
         public static final int image=0x7f090004;
+        public static final int relativeLayout1=0x7f090005;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
